d428c6b2f61c7d0943886b72ec4df6a6e203c551
Antonio Iannopollo <antonio@eecs.berkeley.edu>
2015-01-22 14:50:08 -0800

    first version of observer generic pattern

---
+++ cool/observer.py
@@ -0,0 +1,56 @@
+"""
+This module define a generic version of the observer design pattern
+"""
+from abc import ABCMeta, abstractmethod
+
+
+class Subject:
+    """
+    Define a subject
+    """
+
+    __metaclass__ = ABCMeta
+
+    def __init__(self):
+        """initialize internal data structures """
+        self.observers = set()
+
+    def attach(self, observer):
+        """attach a new observer to the subject"""
+        self.observers.add(observer)
+
+    def detach(self, observer):
+        """detach a previusly attached observer from the subject"""
+        try:
+            self.observers.remove(observer)
+        except:
+            raise
+
+    def notify(self):
+        """Notify observers that something changed"""
+        for observer in self.observers:
+            observer.notify(self)
+
+    @abstractmethod
+    def get_state(self):
+        """retrieve subject state"""
+        pass
+
+    @abstractmethod
+    def set_state(self):
+        """set subject state"""
+        pass
+
+class Observer(object):
+    """
+    Define a Observer
+    """
+    __metaclass__ = ABCMeta
+
+    @abstractmethod
+    def update(self):
+        """receive an update from a subject"""
+        pass
+
+
+
