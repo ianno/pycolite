a6baf0a5202030abf57b69fb2ab478ef0e37d5a9
Antonio Iannopollo <antonio@berkeley.edu>
2015-02-12 18:33:04 -0800

    added logger configuration in init

--- cool/__init__.py
+++ cool/__init__.py
@@ -3,3 +3,20 @@ Created on Aug 19, 2014
 
 @author: antonio
 '''
+
+import logging
+
+log = logging.getLogger()
+log.setLevel(logging.DEBUG)
+
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+
+# create formatter and add it to the handlers
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+ch.setFormatter(formatter)
+
+# add the handlers to the logger
+log.addHandler(ch)
+
+log.debug('INIT')
